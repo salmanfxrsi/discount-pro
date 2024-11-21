74690aa8e3cc51bacf4c14b234aca37cc6d656aa
salmanfxrsi <salmanxprivate@gmail.com>
2024-11-21 23:56:14 +0600

    feat: update slider image

--- src/components/BannerSlider.jsx
+++ src/components/BannerSlider.jsx
@@ -3,7 +3,7 @@ const BannerSlider = () => {
     <div className="carousel rounded-3xl h-[400px]">
       <div id="slide1" className="carousel-item relative w-full">
         <img
-          src="https://i.ibb.co.com/1XkkHLH/melting-mike-logo-text-design-1308-72745.jpg"
+          src="https://i.ibb.co.com/sPbR3BX/Blog-Banner-main-450-voucher.webp"
           className="w-full"
         />
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
@@ -17,7 +17,7 @@ const BannerSlider = () => {
       </div>
       <div id="slide2" className="carousel-item relative w-full">
         <img
-          src="https://i.ibb.co.com/27gwHFk/sign-that-says-winter-sale-is-available-657162-2236.jpg"
+          src="https://i.ibb.co.com/vqgp3MQ/white-background-white-screen-blank-page-bright-white-5.jpg"
           className="w-full"
         />
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
@@ -31,7 +31,7 @@ const BannerSlider = () => {
       </div>
       <div id="slide3" className="carousel-item relative w-full">
         <img
-          src="https://i.ibb.co.com/Gd29kB8/group-penguins-holding-christmas-banner-23-2148353842.jpg"
+          src="https://i.ibb.co.com/tXLR96V/GToeb5-AXMAAw-I7i.jpg"
           className="w-full"
         />
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
@@ -45,7 +45,7 @@ const BannerSlider = () => {
       </div>
       <div id="slide4" className="carousel-item relative w-full">
         <img
-          src="https://i.ibb.co.com/1MdW5zQ/winter-landscape-concept-flat-design-23-2148352277.jpg"
+          src="https://i.ibb.co.com/xgQzpWh/daraz-free-delivery-bd.webp"
           className="w-full"
         />
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
